ce05fb823be8720854baf91e147dce795d254da8
pfrancis <pfrancis@citywire.co.uk>
2022-01-18 13:44:25 +0000

    api tests

---
+++ APITestProject/DataModel/CreateInstructionDto.cs
@@ -0,0 +1,17 @@
+﻿namespace APITestProject.DataModel
+{
+    public class CreateInstructionDto
+    {
+        public string instructionRef { get; set; }
+        public string customerRef { get; set; }
+        public string direction { get; set; }
+        public string financialInstrumentId { get; set; }
+        public string amount { get; set; }
+        public string currency { get; set; }
+        public string country { get; set; }
+        public string settledByDate { get; set; }
+        public string schemeId { get; set; }
+        public Metadata metadata { get; set; }
+    }
+ }
+
